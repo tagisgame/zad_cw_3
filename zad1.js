f5553b0c4250cf1c273239302212f468b7b07166
Jakub <tagisgame@gmail.com>
2019-05-13 10:06:21 +0200

    Dodanie readline-sync i szablonu zadania

---
+++ zad1.js
@@ -0,0 +1,18 @@
+// <editor-fold desc="Includowanie readline-sync">
+const readlineSync = require('readline-sync');
+// </editor-fold>
+
+//Zmienna finalnie wyświetlana jako wynik działania programu
+var result = "";
+
+// <editor-fold desc="Input">
+//Inputy podawane przez użytkownika
+
+// </editor-fold>
+
+// <editor-fold desc="Operacje wykonywane przez program">
+
+// </editor-fold>
+
+//Wyświetlanie wyniku działania
+console.log(result);
