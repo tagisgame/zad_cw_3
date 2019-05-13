6038776cbc5eb38b0b7cd466c7b9779383ca7e26
Jakub <tagisgame@gmail.com>
2019-05-13 11:00:17 +0200

    Zadanie ukończone

---
+++ zad2.js
@@ -0,0 +1,28 @@
+//Zmienna finalnie wyświetlana jako wynik działania programu
+var result = "";
+
+// <editor-fold desc="Operacje wykonywane przez program">
+var tab = [];
+
+//Losowanie liczb
+result = "Wylosowane liczby: ";
+for (let i = 0; i < 20; i++) {
+  let randNum = Math.floor(Math.random() * 10) + 1;
+  tab[i] = randNum;
+  result += randNum + " ";
+}
+
+//Liczenie wystąpień
+result += "\nWystąpienia:\n";
+for (let i = 0; i < 10; i++) {
+  let count = 0;
+  for (let j = 0; j < 20; j++) {
+    tab[j] === i ? count++ : 0;
+  }
+  
+  result += i + " - " + count + "\n";
+}
+// </editor-fold>
+
+//Wyświetlanie wyniku działania
+console.log(result);
