f62af4c829723b5de2eaae7ae408962de0ba4c1a
Jakub <tagisgame@gmail.com>
2019-05-14 08:33:49 +0200

    Poprawiono komentarze, czytelność i sensowność kodu.

--- zad3.js
+++ zad3.js
@@ -2,55 +2,70 @@
 var result = "";
 
 // <editor-fold desc="Operacje wykonywane przez program">
+// Macierz m
 var m = [];
 
-//Losowanie liczb
-result = "Wylosowana macierz:\n";
-for (let y = 0; y < 5; y++) {
-  m[y] = [];
-  for (let x = 0; x < 5; x++) {
-    //Mnożnik określający czy liczba będzie ujemna
+// <editor-fold desc="Tworzenie macierzy">
+// Tworzenie macierzy i wypełnienie jej losowymi wartościami
+for (let x = 0; x < 5; x++) {
+  m[x] = [];
+  for (let y = 0; y < 5; y++) {
+    // Zmienna określająca znak losowanej liczby
     let randSign = Math.random() < 0.5 ? -1 : 1;
-    //Losowa liczba
-    let randNum = randSign * Math.ceil(Math.random() * 5);
 
-    m[y][x] = randNum
-    result += ((randNum < 0) ? " " : "  ") + randNum;
+    // Losowana liczba z zakresu od -5 do 5 włącznie
+    let randNum = randSign * Math.round(Math.random() * 5);
+
+    // Wpisanie wartości do komórki (x,y)
+    m[x][y] = randNum;
+  }
+}
+// Konstrukcja wiadomości, aby wyświetlić macierz
+result += "Wylosowana macierz:\n";
+for (let y = 0; y < 5; y++) {
+  for (let x = 0; x < 5; x++) {
+    result += (m[x][y] < 0 ? " " : "  ") + m[x][y];
   }
   result += "\n";
 }
-console.log(m)
+// </editor-fold>
 
-//Minimalne wartości w wierszach i maksymalne w kolumnach
-result += "Minimalne wartości w wierszach:\n";
+// <editor-fold desc="Wartości min, max">
+// Wyznaczanie min wier. i max kol. oraz konstrukcja wiadomości
+var rowMin = 6;
+var colMax = -6;
+
+result += "Minimalne wartości w wierszach:\n"
 for (let y = 0; y < 5; y++) {
-  let min = Number.MAX_VALUE;
   for (let x = 0; x < 5; x++) {
-    if (m[y][x] < min) min = m[x][y];
+    if (m[x][y] < rowMin) rowMin = m[x][y];
+    console.log(m[x][y], rowMin);
   }
-
-  result += y + ":" + ((min < 0) ? " " : "  ") + min + "\n";
+  result += y + (rowMin < 0 ? ": " : ":  ") + rowMin + "\n";
+  rowMin = 6;
 }
 
-result += "Maksymalne wartości w kolumnach:\n";
+result += "Maksymalne wartości w kolumnach:\n"
 for (let x = 0; x < 5; x++) {
-  let max = Number.MIN_VALUE;
   for (let y = 0; y < 5; y++) {
-    if (m[y][x] > max) max = m[x][y];
+    if (m[x][y] > colMax) colMax = m[x][y];
+    console.log(m[x][y], colMax);
   }
-
-  result += x + ":" + ((max < 0) ? " " : "  ") + max + "\n";
+  result += x + (colMax < 0 ? ": " : ":  ") + colMax + "\n";
+  colMax = -6;
 }
 
-//Maks d1, min d2
-var max = Number.MIN_VALUE;
-var min = Number.MAX_VALUE;
+// Wyznaczanie max i min przękątnych oraz konstrukcja wiadomości
+var d1Max = -6;
+var d2Min = 6;
+
 for (let i = 0; i < 5; i++) {
-  if (m[i][i] > max) max = m[i][i];
-  if (m[4-i][i] < min) min = m[4-i][i];
+  if (m[i][i] > d1Max) d1Max = m[i][i];
+  if (m[i][4-i] < d2Min) d2Min = m[i][4-i];
 }
-result += "Maksimum na 1. przekątnej: " + max + "\n";
-result += "Minimum na 2. przekątnej: " +  min;
+result += "Maksimum na 1. przekątnej:" + (d1Max < 0 ? "" : " ") + d1Max;
+result += "\nMinimum na 2. przekątnej:" + (d2Min < 0 ? " " : "  ") + d2Min;
+// </editor-fold>
 
 // </editor-fold>
 
